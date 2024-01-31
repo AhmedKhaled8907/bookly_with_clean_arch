20276b9536ed1d30c354d0fcace8a776e0866da3
Ahmed khaled <ahmedkh8907@gmail.com>
2024-02-01 01:27:36 +0200

    bookly UI done

---
+++ lib/Features/home/presentation/views/widgets/featured_list_view.dart
@@ -0,0 +1,31 @@
+import 'package:bookly_with_clean_arch/core/utils/app_router.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import 'custom_book_item.dart';
+
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(right: 12),
+              child: GestureDetector(
+                onTap: () {
+                  GoRouter.of(context).push(
+                    AppRouter.kBookDetailsView,
+                  );
+                },
+                child: const CustomBookImage(),
+              ),
+            );
+          }),
+    );
+  }
+}
---
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../../../../../core/utils/styles.dart';
+import 'best_seller_list_view.dart';
+import 'custom_app_bar.dart';
+import 'featured_list_view.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomAppBar(),
+                Text(
+                  'Featured Books',
+                  style: Styles.textStyle24,
+                ),
+                SizedBox(height: 20),
+                FeaturedBooksListView(),
+                SizedBox(height: 50),
+                Text(
+                  'Best Seller',
+                  style: Styles.textStyle24,
+                ),
+                SizedBox(height: 20),
+              ],
+            ),
+          ),
+          SliverFillRemaining(
+            child: BestSellerListView(),
+          ),
+        ],
+      ),
+    );
+  }
+}
