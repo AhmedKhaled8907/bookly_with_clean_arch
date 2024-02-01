8eff310b8f6d11e1cf4d42eafa61bdfade8662f7
Ahmed khaled <ahmedkh8907@gmail.com>
2024-02-01 02:03:23 +0200

    Add Either class

--- lib/Features/home/domain/repos/home_repo.dart
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -1,6 +1,9 @@
-import 'package:bookly_with_clean_arch/Features/home/domain/entities/book_entity.dart';
+import 'package:dartz/dartz.dart';
+
+import 'package:bookly_with_clean_arch/core/error/failure.dart';
+import '../entities/book_entity.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFeaturedBooks();
-  Future<List<BookEntity>> fetchNewestBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks();
 }
