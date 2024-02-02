6412c607f789ef365cee7abb5bf5d1e7903d6dd2
Ahmed khaled <ahmedkh8907@gmail.com>
2024-02-02 03:30:57 +0200

    implement fetch featured books & fetch newest books

--- lib/Features/home/presentation/manger/cubits/featured_books_cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/manger/cubits/featured_books_cubit/featured_books_cubit.dart
@@ -1,11 +1,24 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_with_clean_arch/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly_with_clean_arch/Features/home/domain/use_cases/fetch_featured_books_use_case.dart';
 import 'package:flutter/foundation.dart';
-
-
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.fetchFeaturedBooksUseCase)
+      : super(FeaturedBooksInitial());
+
+  final FetchFeaturedBooksUseCase fetchFeaturedBooksUseCase;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+
+    final result = await fetchFeaturedBooksUseCase.call();
+
+    result.fold(
+      (failure) => emit(FeaturedBooksFailure(failure.errMessage)),
+      (books) => emit(FeaturedBooksSuccess(books)),
+    );
+  }
 }
---
+++ lib/Features/home/presentation/manger/cubits/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+import '../../../../domain/entities/book_entity.dart';
+import '../../../../domain/use_cases/fetch_newest_books_use_case.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.fetchNewestBooksUseCase) : super(NewestBooksInitial());
+
+  final FetchNewestBooksUseCase fetchNewestBooksUseCase;
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+
+    final result = await fetchNewestBooksUseCase.call();
+
+    result.fold(
+      (failure) => emit(NewestBooksFailure(failure.errMessage)),
+      (books) => emit(NewestBooksSuccess(books)),
+    );
+  }
+}
