da76835aab3810eea616f4fc18850a952be92427
Ahmed khaled <ahmedkh8907@gmail.com>
2024-02-02 03:56:39 +0200

    setup service locator using get it

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
-import 'package:bookly_with_clean_arch/Features/home/domain/entities/book_entity.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
+import 'Features/home/data/repos/home_repo_impl.dart';
+import 'Features/home/domain/entities/book_entity.dart';
+import 'Features/home/domain/use_cases/fetch_featured_books_use_case.dart';
+import 'Features/home/domain/use_cases/fetch_newest_books_use_case.dart';
+import 'Features/home/presentation/manger/cubits/featured_books_cubit/featured_books_cubit.dart';
+import 'Features/home/presentation/manger/cubits/newest_books_cubit/newest_books_cubit.dart';
 import 'constants.dart';
 import 'core/utils/app_router.dart';
+import 'core/utils/functions/setup_service_locator.dart';
 
 void main() async {
   await Hive.initFlutter();
@@ -12,6 +19,7 @@ void main() async {
   await Hive.openBox(kFeaturedBox);
   await Hive.openBox(kNewestBox);
 
+  setupServiceLocator();
   runApp(const Bookly());
 }
 
@@ -20,12 +28,31 @@ class Bookly extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(
+            FetchFeaturedBooksUseCase(
+              getIt.get<HomeRepoImpl>(),
+            ),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => NewestBooksCubit(
+            FetchNewestBooksUseCase(
+              getIt.get<HomeRepoImpl>(),
+            ),
+          ),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
       ),
     );
   }
